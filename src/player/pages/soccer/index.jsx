ef5aa737e80599525b4aef9be063e3248ac2b0a1
AyoMoses1 <ayomoses111@gmail.com>
2023-08-18 16:22:46 +0100

    Work on displaying no data

--- src/player/pages/soccer/index.jsx
+++ src/player/pages/soccer/index.jsx
@@ -1,8 +1,30 @@
-import { Box } from '@chakra-ui/react';
+import { Box, Flex } from '@chakra-ui/react';
 import SoccerBox from 'common/SoccerBox';
 import { results } from 'pages/agents/components/data';
+import { AiOutlineInbox } from 'react-icons/ai';
 import { useLocation } from 'react-router-dom';
 import { useGetAllEventsBySportsAndLeague } from './queryHooks';
+import { Text } from '@chakra-ui/react';
+
+const NoDataBox = () => {
+  return (
+    <Flex
+      align="center"
+      justify="center"
+      h="200px"
+      bg="gray.100"
+      borderRadius="md"
+      boxShadow="md"
+      m={8}
+      flexDirection="column"
+    >
+      <Box as={AiOutlineInbox} color="gray.500" fontSize="5xl" />
+      <Text mt={4} color="gray.600" fontSize="lg">
+        No data available.
+      </Text>
+    </Flex>
+  );
+};
 
 const Index = () => {
   const location = useLocation();
@@ -12,9 +34,11 @@ const Index = () => {
   });
   return (
     <Box>
-      {data?.results?.map((item) => (
-        <SoccerBox data={item} key={item.id} />
-      ))}
+      {data?.results.length ? (
+        data?.results?.map((item) => <SoccerBox data={item} key={item.id} />)
+      ) : (
+        <NoDataBox />
+      )}
     </Box>
   );
 };
